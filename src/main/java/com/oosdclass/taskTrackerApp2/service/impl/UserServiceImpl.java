c7112e96fb51851dfe281283c30b9549aaf0720c
Snap IT Solutions LLC <ram@snapit.solutions>
2017-12-19 15:30:39 -0600

    Service Layer code for validating users

--- src/main/java/com/oosdclass/taskTrackerApp2/service/impl/UserServiceImpl.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/service/impl/UserServiceImpl.java
@@ -1,5 +1,30 @@
 package com.oosdclass.taskTrackerApp2.service.impl;
 
-public class UserServiceImpl {
+import com.oosdclass.taskTrackerApp2.model.User;
+import com.oosdclass.taskTrackerApp2.service.UserService;
+
+public class UserServiceImpl implements UserService {
+
+	@Override
+	public boolean isUserValid(User user) {
+		
+		User mockUser = mockUserDAO();
+		
+		if(user.getUsername().equals(mockUser.getUsername()) &&
+				user.getPassword().equals(mockUser.getPassword())) {
+			return true;
+		}
+		else {
+			return false;	
+		}
+		
+	}
+	
+	public User mockUserDAO() {
+		User user = new User();
+		user.setUsername("admin");
+		user.setPassword("pass");
+		return user;
+	}
 
 }

8602290a6be2c4a03671f3dca297519ef3ce2842
Snap IT Solutions LLC <ram@snapit.solutions>
2017-12-19 16:08:06 -0600

    Baisc DAO

--- src/main/java/com/oosdclass/taskTrackerApp2/dao/impl/UserDAOImpl.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/dao/impl/UserDAOImpl.java
@@ -1,5 +1,43 @@
 package com.oosdclass.taskTrackerApp2.dao.impl;
 
-public class UserDAOImpl {
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.stereotype.Repository;
+
+import com.oosdclass.taskTrackerApp2.dao.UserDAO;
+import com.oosdclass.taskTrackerApp2.model.User;
+
+@Repository
+public class UserDAOImpl implements UserDAO {
+	
+	//userDAOImpl is dependent on jdbc template 
+	//so we need to inject the dependency
+
+	//jdbc template is a class provided by spring jdbc 
+	//to perform CRUD and map data in rows to objects
+	private JdbcTemplate jdbcTemplate;
+
+	public void setJdbcTemplate(JdbcTemplate jdbcTemplate) {
+		this.jdbcTemplate = jdbcTemplate;
+	}
+
+	@Override
+	public User retrieveByUserName(String username) {
+		//this is the sql query that we want to run on the db
+		String sql = "select * from user where username=?";
+		User user = (User) jdbcTemplate.queryForObject(sql, new Object[] { username }, new RowMapper<User>() {
+			@Override
+			public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+				User user = new User();
+				user.setUsername(rs.getString(1));
+				user.setPassword(rs.getString(2));
+				return user;
+			}
+		});
+		return user;
+	}
 
 }
--- src/main/java/com/oosdclass/taskTrackerApp2/service/impl/UserServiceImpl.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/service/impl/UserServiceImpl.java
@@ -1,14 +1,23 @@
 package com.oosdclass.taskTrackerApp2.service.impl;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.oosdclass.taskTrackerApp2.dao.UserDAO;
 import com.oosdclass.taskTrackerApp2.model.User;
 import com.oosdclass.taskTrackerApp2.service.UserService;
 
+@Service
 public class UserServiceImpl implements UserService {
 
+	@Autowired
+	UserDAO userDAO;
+
+	
 	@Override
 	public boolean isUserValid(User user) {
 		
-		User mockUser = mockUserDAO();
+		User mockUser = userDAO.retrieveByUserName(user.getUsername());
 		
 		if(user.getUsername().equals(mockUser.getUsername()) &&
 				user.getPassword().equals(mockUser.getPassword())) {

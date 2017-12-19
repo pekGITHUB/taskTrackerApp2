22f46b619236235c0b6158abb071b438739abdd5
Snap IT Solutions LLC <ram@snapit.solutions>
2017-12-19 15:19:19 -0600

    basic login page functionality for the app

--- src/main/java/com/oosdclass/taskTrackerApp2/controller/UserController.java
+++ src/main/java/com/oosdclass/taskTrackerApp2/controller/UserController.java
@@ -1,5 +1,45 @@
 package com.oosdclass.taskTrackerApp2.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.servlet.ModelAndView;
+
+import com.oosdclass.taskTrackerApp2.model.User;
+
+
+
+@Controller
 public class UserController {
+	
+	
+	// (/) method (GET) is the FIRST method that is called when the user accesses
+	// our app using
+	// the root context and display's the home page, which is nothing but "login"
+	//when the user accessses the login page we are initializing User object for the user to fill in
+	@RequestMapping(value = "/")
+	public ModelAndView login(ModelAndView model) {
+			User user = new User();
+			//by adding object we are informing 
+			//spring mvc to initialize user object on the login page
+			model.addObject(user);
+			//by setting viewname to home spring mvc displays 
+			//the jsp that is name home
+			model.setViewName("home");
+			return model;
+		}
+	
+		//(/login) method (POST) is mapped to /login URL where user post/submits the
+		// form which is mapped to user object and we pass to service
+		@RequestMapping(value = "/login", method = RequestMethod.POST)
+		public ModelAndView login(User userLoginFormObject) {
+			System.out.println("username captured from the user is:" + userLoginFormObject.getUsername());
+			System.out.println("password captured from the user is:" + userLoginFormObject.getPassword());
+			
+			ModelAndView model = new ModelAndView();
+			model.setViewName("viewTask");
+			return model;
+		}
+		
 
 }
